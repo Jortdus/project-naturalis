a34f660a220f19de85d0bdd34da539a685563a99
Steinberg99 <stein.bergervoet@gmail.com>
2022-01-17 11:38:16 +0100

    Add .prettierrc

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Categories from "./Components/Categories";
 import Objects from "./Components/Objects";
 
-
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Categories />} />
-        <Route path="/detail" element={<Objects />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Categories />} />
+                <Route path="/objects" element={<Objects />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
