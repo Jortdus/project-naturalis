72897448a23931baaf7795b421166192e77888d1
Steinberg99 <stein.bergervoet@gmail.com>
2022-01-19 15:20:03 +0100

    Add random placement of bubbles

--- src/Components/Objects/Canvas/Canvas.js
+++ src/Components/Objects/Canvas/Canvas.js
@@ -4,29 +4,56 @@ import "./Canvas.css";
 import fetchData from "../../../Utils/fetchData.js";
 
 const url = "https://api.gbif.org/v1/occurrence/";
+const baseX = 2;
+const baseY = 3;
+
 let bubbles = [];
+let lastMousePosition = null;
 
-function Canvas({ gbifIds, setSelectedGbifObject }) {
+function Canvas({ gbifIds, setGbifObject }) {
     const setup = (p5, canvasParentRef) => {
-        p5.createCanvas(window.innerWidth, window.innerHeight).parent(
-            canvasParentRef
-        );
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        let canvasFactor = 1;
 
-        const minRadius = 25;
-        const maxRadius = 50;
-        gbifIds.forEach((gbifId) => {
-            let x = Math.random() * window.innerWidth;
-            let y = Math.random() * window.innerHeight;
-            let r = Math.random() * (maxRadius - minRadius) + minRadius;
-            bubbles.push(new Bubble(x, y, r, gbifId));
+        // Create the canvas
+        p5.createCanvas(width, height).parent(canvasParentRef);
+
+        // This is kind of bad but it works for now
+        if (gbifIds.length <= 15) {
+            canvasFactor = 0.5;
+        } else if (gbifIds.length >= 100) {
+            canvasFactor = 2;
+        }
+
+        // Create the bubbles based on the provided gbif ids
+        gbifIds.forEach((id, index) => {
+            let x = halton(index, baseX) * width * canvasFactor;
+            let y = halton(index, baseY) * height * canvasFactor;
+            // This is also kind of bad but it works for now
+            if (canvasFactor === 2) {
+                x -= width / 2;
+                y -= height / 2;
+            } else if (canvasFactor === 0.5) {
+                x += width * 0.25;
+                y += height * 0.25;
+            }
+            let r = 30; // Circle radius
+            bubbles.push(new Bubble(x, y, r, id)); // Push
         });
 
-        p5.noLoop();
+        p5.frameRate(30);
     };
 
     const draw = (p5) => {
-        p5.background(102, 102, 255);
+        // Draw the background of the canvas
+        p5.background(255, 255, 250);
+
+        // Draw the bubbles onto the canvas
         bubbles.forEach((bubble) => {
+            p5.stroke(13, 92, 99);
+            p5.strokeWeight(2.5);
+            p5.fill(68, 161, 160);
             p5.ellipse(bubble.x, bubble.y, bubble.r);
         });
     };
@@ -36,22 +63,64 @@ function Canvas({ gbifIds, setSelectedGbifObject }) {
             let d = p5.dist(p5.mouseX, p5.mouseY, bubble.x, bubble.y);
             if (d < bubble.r) {
                 fetchData(url + bubble.id).then((d) => {
-                    setSelectedGbifObject(d);
+                    setGbifObject(d);
                 });
             }
         });
     };
 
-    class Bubble {
-        constructor(x, y, r, id) {
-            this.x = x;
-            this.y = y;
-            this.r = r;
-            this.id = id;
+    const mouseDragged = (p5) => {
+        if (lastMousePosition) {
+            // Calculate the change in the mouse position
+            const change = {
+                x: lastMousePosition.x - p5.mouseX,
+                y: lastMousePosition.y - p5.mouseY
+            };
+            bubbles.forEach((bubble) => {
+                bubble.x -= change.x;
+                bubble.y -= change.y;
+            });
         }
-    }
+        lastMousePosition = {
+            x: p5.mouseX,
+            y: p5.mouseY
+        };
+    };
 
-    return <Sketch setup={setup} draw={draw} mouseClicked={mouseClicked} />;
+    const mouseReleased = () => {
+        lastMousePosition = null;
+    };
+
+    return (
+        <Sketch
+            setup={setup}
+            draw={draw}
+            mouseClicked={mouseClicked}
+            mouseDragged={mouseDragged}
+            mouseReleased={mouseReleased}
+        />
+    );
+}
+
+function halton(index, base) {
+    let result = 0;
+    let f = 1 / base;
+    let i = index;
+    while (i > 0) {
+        result = result + f * (i % base);
+        i = Math.floor(i / base);
+        f = f / base;
+    }
+    return result;
+}
+
+class Bubble {
+    constructor(x, y, r, id) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.id = id;
+    }
 }
 
 export default Canvas;
--- src/Components/Objects/Objects.js
+++ src/Components/Objects/Objects.js
@@ -2,11 +2,8 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import "./Objects.css";
 import data from "../../Data/dataId.json";
-import fetchData from "../../Utils/fetchData.js";
 import Aside from "./Aside/Aside.js";
 import Canvas from "./Canvas/Canvas.js";
-
-const url = "https://api.gbif.org/v1/occurrence/";
 
 function Objects() {
     const [selectedGbifObject, setSelectedGbifObject] = useState(null);
@@ -14,14 +11,6 @@ function Objects() {
     // Retrieve the gbif ids based on the genus from the url
     let { genus } = useParams();
     let genusObject = data.find((e) => e.genus === genus);
-
-    // Function to set the selected gbif object
-    function fetchGbifData(id) {
-        fetchData(url + id).then((d) => {
-            setSelectedGbifObject(d);
-            console.log(d);
-        });
-    }
 
     // Display the Aside component based on the selected gbif object
     let aside;
@@ -36,20 +25,10 @@ function Objects() {
 
     return (
         <div>
-            <div>
-                {/* <h1>{genus}</h1>
-                <ul>
-                    {genusObject.ids.map((id) => (
-                        <li onClick={(e) => fetchGbifData(e.target.innerHTML)}>
-                            {id}
-                        </li>
-                    ))}
-                </ul> */}
-                {aside}
-            </div>
+            <div>{aside}</div>
             <Canvas
                 gbifIds={genusObject.ids}
-                setSelectedGbifObject={setSelectedGbifObject}
+                setGbifObject={setSelectedGbifObject}
             />
         </div>
     );
